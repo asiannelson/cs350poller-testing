749285dfab594741a05afad7e07c5354159b4c00
Nelson <nelson14h@yahoo.com>
2018-04-10 12:23:05 -0600

    nhsiao-hw8

--- polls/models.py
+++ polls/models.py
@@ -7,36 +7,43 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    def get_choices(self):
-        """Returns a list of choice objects associated with this question."""
-        return list(self.choice_set.all())
+	def get_choices(self):
+		"""Returns a list of choice objects associated with this question."""
+		return list(self.choice_set.all())
 
-    def get_leading_choice(self):
-        """Returns the choice object with the most votes."""
-        pass  # Requires implementation!
+	def get_leading_choice(self):
+		"""Returns the choice object with the most votes."""
+		maxvotes = 0
+		leadervotes = ''
+		for i in self.get_choices():		
+			if i.votes > maxvotes:
+				maxvotes = i.votes
+				leadervotes = i
+		return leadervotes
 
-    def get_leading_choice_pct(self):
-        """Returns the percentage of votes for the leading choice."""
-        total_votes = 1.0
-        for choice in self.get_choices():
-            total_votes += votes
-        
-        leader = self.get_leading_choice()
-        return leader.votes / total_votes
+	def get_leading_choice_pct(self):
+		"""Returns the percentage of votes for the leading choice."""
+		total_votes = 0.0 #why start at 1.0 and not 0.0
+		for choice in self.get_choices():
+			total_votes += choice.votes
+		
+		leader = self.get_leading_choice()
+		return leader.votes / total_votes
 
-    def __str__(self):
-        return self.question_text
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.choice_text
+	def __str__(self):
+		return self.choice_text
